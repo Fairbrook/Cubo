a5057e735bc8a07e412da5ddfdd0b02d4ac1ac5c
Fairbrook <32619805+Fairbrook@users.noreply.github.com>
2017-10-08 13:44:01 -0500

    Initial commit

---
+++ Cubo/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Cubo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.acercar = new System.Windows.Forms.Button();
+            this.alejar = new System.Windows.Forms.Button();
+            this.btnRotateX = new System.Windows.Forms.Button();
+            this.btnRotateY = new System.Windows.Forms.Button();
+            this.btnRotateZ = new System.Windows.Forms.Button();
+            this.btnAbajo = new System.Windows.Forms.Button();
+            this.btnArriba = new System.Windows.Forms.Button();
+            this.btnIzq = new System.Windows.Forms.Button();
+            this.btnDer = new System.Windows.Forms.Button();
+            this.lblRotateX = new System.Windows.Forms.Label();
+            this.lblRotateY = new System.Windows.Forms.Label();
+            this.lblRotateZ = new System.Windows.Forms.Label();
+            this.lblZoom = new System.Windows.Forms.Label();
+            this.btnFrente = new System.Windows.Forms.Button();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // acercar
+            // 
+            this.acercar.Location = new System.Drawing.Point(12, 529);
+            this.acercar.Name = "acercar";
+            this.acercar.Size = new System.Drawing.Size(23, 23);
+            this.acercar.TabIndex = 0;
+            this.acercar.TabStop = false;
+            this.acercar.Text = "+";
+            this.acercar.UseVisualStyleBackColor = true;
+            this.acercar.Click += new System.EventHandler(this.acercar_Click);
+            // 
+            // alejar
+            // 
+            this.alejar.Location = new System.Drawing.Point(54, 529);
+            this.alejar.Name = "alejar";
+            this.alejar.Size = new System.Drawing.Size(22, 23);
+            this.alejar.TabIndex = 1;
+            this.alejar.Text = "-";
+            this.alejar.UseVisualStyleBackColor = true;
+            this.alejar.Click += new System.EventHandler(this.alejar_Click);
+            // 
+            // btnRotateX
+            // 
+            this.btnRotateX.Location = new System.Drawing.Point(120, 529);
+            this.btnRotateX.Name = "btnRotateX";
+            this.btnRotateX.Size = new System.Drawing.Size(53, 23);
+            this.btnRotateX.TabIndex = 2;
+            this.btnRotateX.Text = "Rotar X";
+            this.btnRotateX.UseVisualStyleBackColor = true;
+            this.btnRotateX.Click += new System.EventHandler(this.btnRotateX_Click);
+            // 
+            // btnRotateY
+            // 
+            this.btnRotateY.Location = new System.Drawing.Point(199, 529);
+            this.btnRotateY.Name = "btnRotateY";
+            this.btnRotateY.Size = new System.Drawing.Size(54, 23);
+            this.btnRotateY.TabIndex = 3;
+            this.btnRotateY.Text = "Rotar Y";
+            this.btnRotateY.UseVisualStyleBackColor = true;
+            this.btnRotateY.Click += new System.EventHandler(this.btnRotateY_Click);
+            // 
+            // btnRotateZ
+            // 
+            this.btnRotateZ.Location = new System.Drawing.Point(288, 529);
+            this.btnRotateZ.Name = "btnRotateZ";
+            this.btnRotateZ.Size = new System.Drawing.Size(51, 23);
+            this.btnRotateZ.TabIndex = 4;
+            this.btnRotateZ.Text = "Rotar Z";
+            this.btnRotateZ.UseVisualStyleBackColor = true;
+            this.btnRotateZ.Click += new System.EventHandler(this.btnRotateZ_Click);
+            // 
+            // btnAbajo
+            // 
+            this.btnAbajo.Location = new System.Drawing.Point(444, 539);
+            this.btnAbajo.Name = "btnAbajo";
+            this.btnAbajo.Size = new System.Drawing.Size(48, 23);
+            this.btnAbajo.TabIndex = 5;
+            this.btnAbajo.Text = "Abajo";
+            this.btnAbajo.UseVisualStyleBackColor = true;
+            this.btnAbajo.Click += new System.EventHandler(this.btnAbajo_Click);
+            // 
+            // btnArriba
+            // 
+            this.btnArriba.Location = new System.Drawing.Point(444, 480);
+            this.btnArriba.Name = "btnArriba";
+            this.btnArriba.Size = new System.Drawing.Size(48, 23);
+            this.btnArriba.TabIndex = 6;
+            this.btnArriba.Text = "Arriba";
+            this.btnArriba.UseVisualStyleBackColor = true;
+            this.btnArriba.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnIzq
+            // 
+            this.btnIzq.Location = new System.Drawing.Point(388, 509);
+            this.btnIzq.Name = "btnIzq";
+            this.btnIzq.Size = new System.Drawing.Size(58, 23);
+            this.btnIzq.TabIndex = 7;
+            this.btnIzq.Text = "Izquierda";
+            this.btnIzq.UseVisualStyleBackColor = true;
+            this.btnIzq.Click += new System.EventHandler(this.btnIzq_Click);
+            // 
+            // btnDer
+            // 
+            this.btnDer.Location = new System.Drawing.Point(491, 509);
+            this.btnDer.Name = "btnDer";
+            this.btnDer.Size = new System.Drawing.Size(59, 23);
+            this.btnDer.TabIndex = 8;
+            this.btnDer.Text = "Derecha";
+            this.btnDer.UseVisualStyleBackColor = true;
+            this.btnDer.Click += new System.EventHandler(this.btnDer_Click);
+            // 
+            // lblRotateX
+            // 
+            this.lblRotateX.AutoSize = true;
+            this.lblRotateX.Location = new System.Drawing.Point(117, 559);
+            this.lblRotateX.Name = "lblRotateX";
+            this.lblRotateX.Size = new System.Drawing.Size(35, 13);
+            this.lblRotateX.TabIndex = 9;
+            this.lblRotateX.Text = "label1";
+            // 
+            // lblRotateY
+            // 
+            this.lblRotateY.AutoSize = true;
+            this.lblRotateY.Location = new System.Drawing.Point(196, 559);
+            this.lblRotateY.Name = "lblRotateY";
+            this.lblRotateY.Size = new System.Drawing.Size(35, 13);
+            this.lblRotateY.TabIndex = 10;
+            this.lblRotateY.Text = "label1";
+            // 
+            // lblRotateZ
+            // 
+            this.lblRotateZ.AutoSize = true;
+            this.lblRotateZ.Location = new System.Drawing.Point(285, 559);
+            this.lblRotateZ.Name = "lblRotateZ";
+            this.lblRotateZ.Size = new System.Drawing.Size(35, 13);
+            this.lblRotateZ.TabIndex = 11;
+            this.lblRotateZ.Text = "label1";
+            // 
+            // lblZoom
+            // 
+            this.lblZoom.AutoSize = true;
+            this.lblZoom.Location = new System.Drawing.Point(12, 559);
+            this.lblZoom.Name = "lblZoom";
+            this.lblZoom.Size = new System.Drawing.Size(35, 13);
+            this.lblZoom.TabIndex = 12;
+            this.lblZoom.Text = "label1";
+            // 
+            // btnFrente
+            // 
+            this.btnFrente.Location = new System.Drawing.Point(594, 480);
+            this.btnFrente.Name = "btnFrente";
+            this.btnFrente.Size = new System.Drawing.Size(36, 23);
+            this.btnFrente.TabIndex = 13;
+            this.btnFrente.Text = "Z+";
+            this.btnFrente.UseVisualStyleBackColor = true;
+            this.btnFrente.Click += new System.EventHandler(this.btnFrente_Click);
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Location = new System.Drawing.Point(594, 529);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(36, 23);
+            this.btnAtras.TabIndex = 14;
+            this.btnAtras.Text = "Z-";
+            this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(12, 13);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 584);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.btnFrente);
+            this.Controls.Add(this.lblZoom);
+            this.Controls.Add(this.lblRotateZ);
+            this.Controls.Add(this.lblRotateY);
+            this.Controls.Add(this.lblRotateX);
+            this.Controls.Add(this.btnDer);
+            this.Controls.Add(this.btnIzq);
+            this.Controls.Add(this.btnArriba);
+            this.Controls.Add(this.btnAbajo);
+            this.Controls.Add(this.btnRotateZ);
+            this.Controls.Add(this.btnRotateY);
+            this.Controls.Add(this.btnRotateX);
+            this.Controls.Add(this.alejar);
+            this.Controls.Add(this.acercar);
+            this.Name = "Form1";
+            this.Text = "Cubo";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button acercar;
+        private System.Windows.Forms.Button alejar;
+        private System.Windows.Forms.Button btnRotateX;
+        private System.Windows.Forms.Button btnRotateY;
+        private System.Windows.Forms.Button btnRotateZ;
+        private System.Windows.Forms.Button btnAbajo;
+        private System.Windows.Forms.Button btnArriba;
+        private System.Windows.Forms.Button btnIzq;
+        private System.Windows.Forms.Button btnDer;
+        private System.Windows.Forms.Label lblRotateX;
+        private System.Windows.Forms.Label lblRotateY;
+        private System.Windows.Forms.Label lblRotateZ;
+        private System.Windows.Forms.Label lblZoom;
+        private System.Windows.Forms.Button btnFrente;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Cubo/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpMatrices;
+using OpMatrices.Types;
+using OpMatrices.Proceso;
+
+namespace Cubo
+{
+    public partial class Form1 : Form
+    {
+        private Matriz<double>[] puntos;
+        private Matriz<double> zoom;
+        private Matriz<double> PP;
+        private Matriz<double> x;
+        private Matriz<double> y;
+        private Matriz<double> z;
+
+        int usrZoom;
+        double usrX;
+        double usrY;
+        double usrZ;
+        int usrPosY;
+        int usrPosX;
+        int usrPosZ;
+        private double[,] aux;
+        public Form1()
+        {
+            usrZoom = 10;
+            usrPosY = 250;
+            usrPosX = 260;
+            usrPosZ = 0;
+            usrX = 100;
+            usrY = 30;
+            usrZ = 0;
+            puntos = new Matriz<double>[16];
+            zoom = new Matriz<double>();
+            x = new Matriz<double>();
+            y = new Matriz<double>();
+            z = new Matriz<double>();
+            PP = new Matriz<double>();
+
+            aux = new double[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j) aux[i, j] = 1;
+                    else aux[i, j] = 0;
+                }
+            }
+
+            aux[0, 3] = 10;
+            aux[1, 3] = 0;
+            aux[2, 3] = 0;
+            puntos[0] = new Matriz<double>();
+            puntos[0].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = 10;
+            aux[2, 3] = 0;
+            puntos[1] = new Matriz<double>();
+            puntos[1].setMatriz(4, 4, aux);
+
+            aux[0, 3] = -10;
+            aux[1, 3] = 0;
+            aux[2, 3] = 0;
+            puntos[2] = new Matriz<double>();
+            puntos[2].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = 0;
+            aux[2, 3] = 10;
+            puntos[3] = new Matriz<double>();
+            puntos[3].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = -10;
+            aux[2, 3] = 0;
+            puntos[4] = new Matriz<double>();
+            puntos[4].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 10;
+            aux[1, 3] = 0;
+            aux[2, 3] = 0;
+            puntos[5] = new Matriz<double>();
+            puntos[5].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = 0;
+            aux[2, 3] = -10;
+            puntos[6] = new Matriz<double>();
+            puntos[6].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = 10;
+            aux[2, 3] = 0;
+            puntos[7] = new Matriz<double>();
+            puntos[7].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = 0;
+            aux[2, 3] = 10;
+            puntos[8] = new Matriz<double>();
+            puntos[8].setMatriz(4, 4, aux);
+
+            aux[0, 3] = -10;
+            aux[1, 3] = 0;
+            aux[2, 3] = 0;
+            puntos[9] = new Matriz<double>();
+            puntos[9].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = -10;
+            aux[2, 3] = 0;
+            puntos[10] = new Matriz<double>();
+            puntos[10].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = 0;
+            aux[2, 3] = -10;
+            puntos[11] = new Matriz<double>();
+            puntos[11].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = 10;
+            aux[2, 3] = 0;
+            puntos[12] = new Matriz<double>();
+            puntos[12].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = 0;
+            aux[2, 3] = 10;
+            puntos[13] = new Matriz<double>();
+            puntos[13].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 10;
+            aux[1, 3] = 0;
+            aux[2, 3] = 0;
+            puntos[14] = new Matriz<double>();
+            puntos[14].setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = -10;
+            aux[2, 3] = 0;
+            puntos[15] = new Matriz<double>();
+            puntos[15].setMatriz(4, 4, aux);
+
+            InitializeComponent();
+        }
+
+        private void pivote()
+        {
+            aux = new double[4, 4];
+            lblRotateX.Text = usrX.ToString();
+            lblRotateY.Text = usrY.ToString();
+            lblRotateZ.Text = usrZ.ToString();
+            lblZoom.Text = usrZoom.ToString();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j) aux[i, j] = 1;
+                    else aux[i, j] = 0;
+                }
+            }
+
+            aux[0, 3] = usrPosX;
+            aux[1, 3] = usrPosY;
+            aux[2, 3] = usrPosZ;
+            PP.setMatriz(4, 4, aux);
+
+            aux[0, 3] = 0;
+            aux[1, 3] = 0;
+            aux[2, 3] = 0;
+            aux[0, 0] = usrZoom;
+            aux[1, 1] = usrZoom;
+            aux[2, 2] = usrZoom;
+            zoom.setMatriz(4, 4, aux);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    aux[i, j] = 0;
+                }
+            }
+            aux[0, 0] = 1;
+            aux[3, 3] = 1;
+            aux[1, 1] = Math.Cos(usrX * (Math.PI / 180));
+            aux[1, 2] = (Math.Sin(usrX * (Math.PI / 180)) * -1);
+            aux[2, 1] = Math.Sin(usrX * (Math.PI / 180));
+            aux[2, 2] = Math.Cos(usrX * (Math.PI / 180));
+            x.setMatriz(4, 4, aux);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    aux[i, j] = 0;
+                }
+            }
+            aux[1, 1] = 1;
+            aux[3, 3] = 1;
+            aux[0, 0] = Math.Cos(usrY * (Math.PI / 180));
+            aux[0, 2] = Math.Sin(usrY * (Math.PI / 180));
+            aux[2, 0] = (Math.Sin(usrY * (Math.PI / 180)) * -1);
+            aux[2, 2] = Math.Cos(usrY * (Math.PI / 180));
+            y.setMatriz(4, 4, aux);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    aux[i, j] = 0;
+                }
+            }
+            aux[2, 2] = 1;
+            aux[3, 3] = 1;
+            aux[0, 0] = Math.Cos(usrZ * (Math.PI / 180));
+            aux[0, 1] = (Math.Sin(usrZ * (Math.PI / 180)) * -1);
+            aux[1, 0] = Math.Sin(usrZ * (Math.PI / 180));
+            aux[1, 1] = Math.Cos(usrZ * (Math.PI / 180));
+            z.setMatriz(4, 4, aux);
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            double x1, y1, x2, y2;
+            Matriz<double> pos;
+            pivote();
+            pos = Operaciones.mult(PP, z);
+            pos = Operaciones.mult(pos, y);
+            pos = Operaciones.mult(pos, x);
+            pos = Operaciones.mult(pos, zoom);
+            x1 = pos.getItem(0, 3) - (Math.Cos(30) * pos.getItem(2, 3));
+            y1 = pos.getItem(1, 3) - (Math.Sin(30) * pos.getItem(2, 3));
+            pos = Operaciones.mult(pos, puntos[0]);
+            for (int i = 1; i < 16; i++)
+            {
+                x2 = pos.getItem(0, 3) - (Math.Cos(30) * pos.getItem(2, 3));
+                y2 = pos.getItem(1, 3) - (Math.Sin(30) * pos.getItem(2, 3));
+                e.Graphics.DrawLine(Pens.IndianRed, Convert.ToInt32(x1), Convert.ToInt32(y1), Convert.ToInt32(x2), Convert.ToInt32(y2));
+                pos = Operaciones.mult(pos, puntos[i]);
+                x1 = x2;
+                y1 = y2;
+            }
+            x2 = pos.getItem(0, 3) - (Math.Cos(30) * pos.getItem(2, 3));
+            y2 = pos.getItem(1, 3) - (Math.Sin(30) * pos.getItem(2, 3));
+            e.Graphics.DrawLine(Pens.IndianRed, Convert.ToInt32(x1), Convert.ToInt32(y1), Convert.ToInt32(x2), Convert.ToInt32(y2));
+        }
+
+        private void acercar_Click(object sender, EventArgs e)
+        {
+            usrZoom += 1;
+            this.Refresh();
+        }
+
+        private void alejar_Click(object sender, EventArgs e)
+        {
+            if (usrZoom > 1) usrZoom -= 1;
+            this.Refresh();
+        }
+
+        private void btnRotateX_Click(object sender, EventArgs e)
+        {
+            if (usrX < 360) usrX += 10;
+            else usrX = 0;
+            this.Refresh();
+        }
+
+        private void btnRotateY_Click(object sender, EventArgs e)
+        {
+            if (usrY < 360) usrY += 10;
+            else usrY = 0;
+            this.Refresh();
+        }
+
+        private void btnRotateZ_Click(object sender, EventArgs e)
+        {
+            if (usrZ < 360) usrZ += 10;
+            else usrZ = 0;
+            this.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (usrPosY > 50) usrPosY -= 50;
+            this.Refresh();
+        }
+
+        private void btnAbajo_Click(object sender, EventArgs e)
+        {
+            usrPosY += 50;
+            this.Refresh();
+        }
+
+        private void btnIzq_Click(object sender, EventArgs e)
+        {
+            if (usrPosX > 50) usrPosX -= 50;
+            this.Refresh();
+        }
+
+        private void btnDer_Click(object sender, EventArgs e)
+        {
+            usrPosX += 50;
+            this.Refresh();
+        }
+
+        private void btnFrente_Click(object sender, EventArgs e)
+        {
+            usrPosZ+=50;
+            this.Refresh();
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            if (usrPosZ > 1) usrPosZ-=50;
+            this.Refresh();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            usrZoom = 10;
+            usrPosY = 250;
+            usrPosX = 260;
+            usrPosZ = 0;
+            usrX = 100;
+            usrY = 30;
+            usrZ = 0;
+            this.Refresh();
+        }
+
+
+    }
+}
